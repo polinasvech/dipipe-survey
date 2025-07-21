ad17aa9ba9869f020060f46dcd4a34738f72b219
andrey <yaidf@ya.ru>
2025-07-21 17:37:41 +0500

    404 page

--- app_client/app/main.py
+++ app_client/app/main.py
@@ -77,5 +77,8 @@ def get_survey_by_id(uuid):
         ]
     }
     return jsonify(survey)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
