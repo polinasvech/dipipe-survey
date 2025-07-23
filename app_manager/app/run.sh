3005ac291cdef4394c65894199ef25684fdf480b
polinasvech <polina.svech1919@gmail.com>
2025-07-23 08:39:13 +0500

    fixed run scrips

--- app_manager/app/run.sh
+++ app_manager/app/run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Запуск парсинга xlsx..."
 export PYTHONPATH=/opt/app/app
+
+#echo "Запуск парсинга xlsx..."
 #python parse_excel.py
+
+echo "Запуск приложения..."
 uvicorn main:app --host 0.0.0.0 --port 8002 --reload
